41f5f6e56ea70f71f53c2fedba4a8fd9ac252239
ibadkxan <ibadkxan@gmail.com>
2026-04-14 00:22:42 +0530

    js practise set

---
+++ practise/1.js
@@ -0,0 +1,14 @@
+// alert('hey there');
+console.log("hey there");
+console.warn("hey there");
+console.error("hey there");
+alert("JavaScript is Magical");
+// document.body.innerHTML = "Hello World";
+let a = 10;
+console.log(a);
+console.log(typeof 10);
+a = 6;
+console.log(a);
+const b = 20;
+console.log(b);
+// b=30; This will throw an error because 'b' is a constant
